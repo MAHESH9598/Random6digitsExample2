61456e286b641f2bb6c6bd83e0452a1e96db8cbd
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-15 12:03:47 +0530

    Initial commit

---
+++ Random6digitsExample2.py
@@ -0,0 +1,3 @@
+""" This Program again why i wrote , for getting output without spaces between the digits"""
+from random import*
+print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
